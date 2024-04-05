127c807edd287764baaf728b263f3117cba892b5
Wilson Yeung <wilson.yeung@hkland.com>
2024-04-05 17:33:10 +0800

    update

--- question_domain/question_domain.go
+++ question_domain/question_domain.go
@@ -6,7 +6,7 @@ type Question struct {
 	PublishYear     int    `json:"publishYear" db:"publish_year"`
 	SchoolCodeId    int    `json:"schoolCodeId" db:"school_code_id"`
 	GradeId         int    `json:"gradeId" db:"grade_id"`
-	Language        string `json:"language" db:"language"`
+	Language        int    `json:"language" db:"language"`
 	Question        string `json:"question" db:"question"`
 	Answer          string `json:"answer" db:"answer"`
 	Media           string `json:"media" db:"media"`
